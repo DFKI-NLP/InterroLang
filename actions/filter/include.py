9bbe64d994c5caf85dbf1d5ca421ca75a6d19fd2
tatianak <36244-tatianak@users.noreply.gitlab.aicrowd.com>
2023-03-16 11:48:46 +0100

    added include operation

---
+++ actions/filter/include.py
@@ -0,0 +1,39 @@
+"""The include operation.
+
+This operation checks whether the input string appears in the dataset.
+"""
+import re
+
+def includes_operation(conversation, text_to_match):
+    """The include operation."""
+    
+    temp_dataset = conversation.temp_dataset.contents["X"]
+    text_inputs = temp_dataset["text"]
+    
+    threshold = 5 # num of words before and after the match
+    output_str = ""
+    match_len = len(text_to_match)
+    for inum, text_input in enumerate(text_inputs):
+        matched_indices = [m.start() for m in re.finditer(text_to_match, text_input, re.IGNORECASE)]
+        for match_idx in matched_indices:
+            before = text_input[:match_idx] 
+            match = text_to_match
+            after = text_input[match_idx+match_len:]
+            before = ' '.join(before.split()[-threshold:])
+            after  = ' '.join(after.split()[:threshold])
+            idx = temp_dataset["idx"][inum]
+            output_str += f"idx {idx}: "
+            if len(before)>0:
+                output_str+=f"... {before} "
+            output_str+=f"<b>{match}</b>"
+            if len(after)>0:
+                output_str+=f" {after} ..."
+            output_str+=" <br>"
+    if len(output_str)==0:
+        output_str = "No matches were found for: "+text_to_match+"<br>"
+    else:
+        output_str = f"I found the following matches for <b>{text_to_match}</b>: <br>"+output_str
+        
+    return output_str
+
+
