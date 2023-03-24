b7a60459e21a55309deb6381818844e8e637488f
qiaw99 <872907968@qq.com>
2023-03-24 16:49:33 +0100

    formalization

--- actions/prediction/prediction_likelihood.py
+++ actions/prediction/prediction_likelihood.py
@@ -12,7 +12,7 @@ The model predicts the instance with <b>{filter_string}</b> as:
 """
 
 
-def handle_input(parse_text):
+def extract_id_from_parse_text(parse_text):
     """
 
     Args:
@@ -60,7 +60,7 @@ def predict_likelihood(conversation, parse_text, i, **kwargs):
 
     # Get the dataset name
     name = conversation.describe.get_dataset_name()
-    instance_id = handle_input(parse_text)
+    instance_id = extract_id_from_parse_text(parse_text)
     model_predictions, model_prediction_probabilities = get_predictions_and_probabilities(name, instance_id)
 
     return_s = f"For instance with id <b>{instance_id}</b>: "
@@ -68,64 +68,12 @@ def predict_likelihood(conversation, parse_text, i, **kwargs):
 
     # Go through all classes
     for _class in range(len(model_prediction_probabilities)):
-        classs_name = conversation.get_class_name_from_label(_class)
+        class_name = conversation.get_class_name_from_label(_class)
         prob = round(model_prediction_probabilities[_class] * 100, conversation.rounding_precision)
         return_s += "<li>"
-        return_s += f"The likelihood of class <b>{classs_name}</b> is <b>{prob}%</b>"
+        return_s += f"The likelihood of class <b>{class_name}</b> is <b>{prob}%</b>"
         return_s += "</li>"
     return_s += "</ul>"
 
     return return_s, 1
 
-    # predict_proba = conversation.get_var('model_prob_predict').contents
-    # model = conversation.get_var('model').contents
-    # data = conversation.temp_dataset.contents['X'].values
-    #
-    # if len(conversation.temp_dataset.contents['X']) == 0:
-    #     return 'There are no instances that meet this description!', 0
-    #
-    # model_prediction_probabilities = predict_proba(data)
-    # model_predictions = model.predict(data)
-    # num_classes = model_prediction_probabilities.shape[1]
-    #
-    # # Format return string
-    # return_s = ""
-    #
-    # filter_string = gen_parse_op_text(conversation)
-    #
-    # if model_prediction_probabilities.shape[0] == 1:
-    #     return_s += f"The model predicts the instance with <b>{filter_string}</b> as:"
-    #     return_s += "<ul>"
-    #     for c in range(num_classes):
-    #         proba = round(model_prediction_probabilities[0, c]*100, conversation.rounding_precision)
-    #         return_s += "<li>"
-    #         if conversation.class_names is None:
-    #             return_s += f"class {str(c)}</b>"
-    #         else:
-    #             class_text = conversation.class_names[c]
-    #             return_s += f"<b>{class_text}</b>"
-    #         return_s += f" with <b>{str(proba)}%</b> probability"
-    #         return_s += "</li>"
-    #     return_s += "</ul>"
-    # else:
-    #     if len(filter_string) > 0:
-    #         filtering_text = f" where <b>{filter_string}</b>"
-    #     else:
-    #         filtering_text = ""
-    #     return_s += f"Over {data.shape[0]} cases{filtering_text} in the data, the model predicts:"
-    #     unique_preds = np.unique(model_predictions)
-    #     return_s += "<ul>"
-    #     for j, uniq_p in enumerate(unique_preds):
-    #         return_s += "<li>"
-    #         freq = np.sum(uniq_p == model_predictions) / len(model_predictions)
-    #         round_freq = str(round(freq*100, conversation.rounding_precision))
-    #
-    #         if conversation.class_names is None:
-    #             return_s += f"<b>class {uniq_p}</b>, <b>{round_freq}%</b>"
-    #         else:
-    #             class_text = conversation.class_names[uniq_p]
-    #             return_s += f"<b>{class_text}</b>, <b>{round_freq}%</b>"
-    #         return_s += " of the time</li>"
-    #     return_s += "</ul>"
-    # return_s += "<br>"
-    # return return_s, 1
