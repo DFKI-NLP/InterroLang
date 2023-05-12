4b72ac21610ffdfae363084a5f2dc98fd7de5198
qiaw99 <872907968@qq.com>
2023-05-12 23:52:42 +0200

    add visualization

--- actions/explanation/adversarial.py
+++ actions/explanation/adversarial.py
@@ -1,25 +1,19 @@
-import json
+import ast
 
 from OpenAttack import attackers, AttackEval
 from OpenAttack.victim import classifiers
 from torch.utils.data import Dataset
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from OpenAttack.utils.visualizer import sent_len, levenshtein_visual
+import numpy as np
 
-import OpenAttack as oa
+from explained_models.ModelABC.DANetwork import DANetwork
+from explained_models.Tokenizer.tokenizer import HFTokenizer
 
 
-class CustomInputDataset(Dataset):
-    def __init__(self, dataset_json, dataset_name):
-        self.data = []
-
-        if dataset_name == "boolq":
-            self.data.append(dataset_json)
-        elif dataset_name == "daily_dialog":
-            pass
-        elif dataset_name == 'olid':
-            pass
-        else:
-            raise NotImplementedError(f"The dataset {dataset_name} is not supported!")
+class AdversarialDataset(Dataset):
+    def __init__(self, dataset_json):
+        self.data = [dataset_json]
 
     def __len__(self):
         return len(self.data)
@@ -58,85 +52,91 @@ def adversarial_operation(conversation, parse_text, i, **kwargs):
 
     for f in f_names[:num_col]:
         filtered_text += texts[f][_id]
-        # dataset_json[f] = texts[f][_id]
         if dataset_name == 'boolq':
             filtered_text += " "
+
     dataset_json['x'] = filtered_text
     dataset_json["y"] = label
-    print(dataset_json)
 
-    dataset = CustomInputDataset(dataset_json, dataset_name)
+    dataset = AdversarialDataset(dataset_json)
 
-    # def dataset_mapping(x):
-    #     return {
-    #         "x": x["question"] + x["passage"],
-    #         "y": x["label"],
-    #     }
-
-    # dataset = dataset.map(function=dataset_mapping)
-
-    model = AutoModelForSequenceClassification.from_pretrained("andi611/distilbert-base-uncased-qa-boolq")
-    tokenizer = AutoTokenizer.from_pretrained("andi611/distilbert-base-uncased-qa-boolq")
-    victim = classifiers.TransformersClassifier(model, tokenizer, model.base_model.embeddings.word_embeddings)
+    if dataset_name == 'boolq':
+        model = AutoModelForSequenceClassification.from_pretrained("andi611/distilbert-base-uncased-qa-boolq")
+        tokenizer = AutoTokenizer.from_pretrained("andi611/distilbert-base-uncased-qa-boolq")
+        victim = classifiers.TransformersClassifier(model, tokenizer, model.base_model.embeddings.word_embeddings)
+    elif dataset_name == 'daily_dialog':
+        model = DANetwork()
+        tokenizer = HFTokenizer('bert-base-uncased', mode='bert').tokenizer
+        victim = classifiers.TransformersClassifier(model.bert, tokenizer, model.bert.base_model.embeddings.word_embeddings)
+    elif dataset_name == 'olid':
+        model = AutoModelForSequenceClassification.from_pretrained("sinhala-nlp/mbert-olid-en")
+        tokenizer = AutoTokenizer.from_pretrained("sinhala-nlp/mbert-olid-en")
+        victim = classifiers.TransformersClassifier(model, tokenizer, model.bert.embeddings.word_embeddings)
+    else:
+        raise NotImplementedError(f"{dataset_name} is not supported!")
 
     attacker = attackers.PWWSAttacker()
+
     # prepare for attacking
     attack_eval = AttackEval(attacker, victim)
+
     # launch attacks and print attack results
     d = attack_eval.eval(dataset, visualize=True)
-    # json_list = [d]
-    # jsonString = json.dumps(json_list)
-    # jsonFile = open("./results.json", "w")
-    # jsonFile.write(jsonString)
-    # jsonFile.close()
+
     x_orig = d["x_orig"]
     x_adv = d["x_adv"]
+    y_orig = ast.literal_eval(d["y_orig"])
+    y_adv = ast.literal_eval(d["y_adv"])
     token_orig = tokenizer.tokenize(x_orig)
     token_adv = tokenizer.tokenize(x_adv)
 
-    # TODO
+    pairs = levenshtein_visual(token_orig, token_adv)
 
-    widths = [
-        (126, 1), (159, 0), (687, 1), (710, 0), (711, 1),
-        (727, 0), (733, 1), (879, 0), (1154, 1), (1161, 0),
-        (4347, 1), (4447, 2), (7467, 1), (7521, 0), (8369, 1),
-        (8426, 0), (9000, 1), (9002, 2), (11021, 1), (12350, 2),
-        (12351, 1), (12438, 2), (12442, 0), (19893, 2), (19967, 1),
-        (55203, 2), (63743, 1), (64106, 2), (65039, 1), (65059, 0),
-        (65131, 2), (65279, 1), (65376, 2), (65500, 1), (65510, 2),
-        (120831, 1), (262141, 2), (1114109, 1),
-    ]
-
-    def char_width(o):
-        if o == 0xe or o == 0xf:
-            return 0
-        for num, wid in widths:
-            if o <= num:
-                return wid
-        return 1
-
-    def sent_len(s):
-        assert isinstance(s, str)
-        ret = 0
-        for it in s:
-            ret += char_width(ord(it))
-        return ret
-
-    assert sent_len(token_orig) == sent_len(token_adv)
     curr1 = ""
     curr2 = ""
-
-    for tokenA, tokenB in zip(token_orig, token_adv):
-        if tokenA.lower() == tokenB.lower():
-            curr1 += tokenA + " "
-            curr2 += tokenB + " "
+    max_len = 200
+    length = 0
+    ret = []
+    for tokenA, tokenB in pairs:
+        assert sent_len(tokenA) == sent_len(tokenB)
+        if length + sent_len(tokenA) + 1 > max_len:
+            ret.append(curr1 + " " * (max_len - length))
+            ret.append(curr2 + " " * (max_len - length))
+            ret.append(" " * max_len)
+            length = sent_len(tokenA) + 1
+            if tokenA.lower() == tokenB.lower():
+                curr1 = tokenA + " "
+                curr2 = tokenB + " "
+            else:
+                curr1 = "<span style='color:red;font-weight:bold'>" + tokenA + "</span>" + " "
+                curr2 = "<span style='color:green;font-weight:bold'>" + tokenB + "</span>" + " "
         else:
-            curr1 += "<b>" + tokenA + "</b>" + " "
-            curr2 += "<b>" + tokenB + "</b>" + " "
+            length += 1 + sent_len(tokenA)
+            if tokenA.lower() == tokenB.lower():
+                curr1 += tokenA + " "
+                curr2 += tokenB + " "
+            else:
+                curr1 += "<span style='color:red;font-weight:bold'>" + tokenA + "</span>" + " "
+                curr2 += "<span style='color:green;font-weight:bold'>" + tokenB + "</span>" + " "
+    if length > 0:
+        ret.append(curr1 + " " * (max_len - length))
+        ret.append(curr2 + " " * (max_len - length))
+        ret.append(" " * max_len)
 
     return_s = ''
-    return_s += curr1
-    return_s += "<br>"
-    return_s += curr2
+
+    if dataset_name == 'boolq' or dataset_name == 'olid':
+        idx_orig = np.argmax(y_orig)
+        prob_orig = round(y_orig[idx_orig] * 100, 2)
+        idx_adv = (~idx_orig.astype(bool)).astype(int)
+        prob_adv = round(y_adv[idx_adv] * 100, 2)
+        return_s += f"<span style='color:green;font-weight:bold'>Label 0 ({prob_orig}%) --> 1 ({prob_adv}%)</span> <br><br>"
+
+    for i in range(0, len(ret)):
+        return_s += ret[i]
+        return_s += "<br>"
+
+        if i % 2 != 0:
+            return_s += '<br>'
 
     return return_s, 1
