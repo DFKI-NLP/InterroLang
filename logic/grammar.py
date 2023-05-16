198ed3f4676f362088d3706368ef0723b1d6a264
nfelnlp <feldhusnlp@gmail.com>
2023-05-17 01:09:54 +0200

    Fix grammar merge conflict.

--- logic/grammar.py
+++ logic/grammar.py
@@ -1,11 +1,7 @@
 GRAMMAR = r"""
 ?start: action
 action: operation done | operation join action | followup done
-<<<<<<< HEAD
-operation: explanation | filter | predictions | whatami | lastturnfilter | lastturnop | data | impfeatures | show | whatif | likelihood | modeldescription | function | score | ndatapoints | label | mistakes | fstats | define | labelfilter | predfilter | includes | globaltopk | cfe | similarity | rationalize | randomprediction
-=======
-operation: explanation | filter | predictions | whatami | lastturnfilter | lastturnop | data | impfeatures | show | likelihood | modeldescription | function | score | ndatapoints | label | mistakes | fstats | define | labelfilter | predfilter | includes | globaltopk | cfe | similarity | rationalize
->>>>>>> main
+operation: explanation | filter | predictions | whatami | lastturnfilter | lastturnop | data | impfeatures | show | likelihood | modeldescription | function | score | ndatapoints | label | mistakes | fstats | define | labelfilter | predfilter | includes | globaltopk | cfe | similarity | rationalize | randomprediction
 
 cfe: " cfe" cfefeature
 cfefeature: {availablefeaturetypes} | " "
