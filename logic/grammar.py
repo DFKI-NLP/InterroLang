a8d3f4c38ad2a5776bbdf75845225064b3d3f53d
nfelnlp <feldhusnlp@gmail.com>
2023-05-16 13:14:41 +0200

    Removed 'whatif' from operation list.

--- logic/grammar.py
+++ logic/grammar.py
@@ -1,7 +1,7 @@
 GRAMMAR = r"""
 ?start: action
 action: operation done | operation join action | followup done
-operation: explanation | filter | predictions | whatami | lastturnfilter | lastturnop | data | impfeatures | show | whatif | likelihood | modeldescription | function | score | ndatapoints | label | mistakes | fstats | define | labelfilter | predfilter | includes | globaltopk | cfe | similarity | rationalize
+operation: explanation | filter | predictions | whatami | lastturnfilter | lastturnop | data | impfeatures | show | likelihood | modeldescription | function | score | ndatapoints | label | mistakes | fstats | define | labelfilter | predfilter | includes | globaltopk | cfe | similarity | rationalize
 
 cfe: " cfe" cfefeature
 cfefeature: {availablefeaturetypes} | " "
