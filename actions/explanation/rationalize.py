12fc322e9401ebb61ea87998ff5317e9f27abed4
nfelnlp <feldhusnlp@gmail.com>
2023-05-20 14:57:24 +0200

    OLID added as an option for rationalize operation.

--- actions/explanation/rationalize.py
+++ actions/explanation/rationalize.py
@@ -16,46 +16,59 @@ def rationalize_operation(conversation, parse_text, i, **kwargs):
             pass
 
     dataset_name = conversation.describe.get_dataset_name()
-    dataset = conversation.temp_dataset.contents['X']
-    model = conversation.get_var('model').contents
+    dataset = conversation.temp_dataset.contents["X"]
+    model = conversation.get_var("model").contents
 
-    if len(conversation.temp_dataset.contents['X']) == 0:
-        return 'There are no instances that meet this description!', 0
+    if len(conversation.temp_dataset.contents["X"]) == 0:
+        return "There are no instances that meet this description!", 0
 
-    return_s = ''
+    return_s = ""
     for idx in id_list:
         instance = dataset.loc[[idx]].values.tolist()[0]
 
+        model_predictions = model.predict(dataset, idx)
+        pred = model_predictions[0]
+
         if dataset_name == "boolq":
-            text = 'Question: ' + instance[0] + '\nPassage: ' + instance[1]
-            label_dict = {0: 'false', 1: 'true'}
-            text_description = 'question and passage'
-            fields = ['question', 'passage']
-            fields_enum = ', '.join([f"'{f}'" for f in fields])
-            output_description = 'answer'
-            model_predictions = model.predict(dataset, idx)
-            pred_str = label_dict[model_predictions[0]]
+            text = "Question: " + instance[0] + "\nPassage: " + instance[1]
+            label_dict = {0: "false", 1: "true"}
+            pred_str = label_dict[pred]
+            intro = f"Answer: {pred_str}"
+            instruction = "Please explain the answer: "
+            max_length = 350
+
+        elif dataset_name == "olid":
+            text = "Tweet: '" + instance[0] + "'"
+            label_dict = {0: "non-offensive", 1: "offensive"}
+            pred_str = label_dict[pred]
+            intro = f"The tweet has been classified as {pred_str}."
+            instruction = "Please explain why: "
+            max_length = 200
+
         else:
             return f"Dataset {dataset_name} currently not supported by rationalize operation", 1
 
         prompt = f"{text}\n" \
-                 f"Based on {text_description}, the {output_description} is {pred_str}. " \
-                 f"Without using {fields_enum}, or revealing the answer or outcome in your response, " \
-                 f"explain why: "
+                 f"{intro}\n" \
+                 f"{instruction}\n"
+        print(f"[rationalize operation]\n=== PROMPT ===\n{prompt}")
 
         input_ids = conversation.decoder.gpt_tokenizer(prompt, return_tensors="pt").input_ids
         input_ids = input_ids.to(device = "cpu")
         generation = conversation.decoder.gpt_model.generate(
             input_ids,
-            max_length=350,
+            max_length=max_length,
             no_repeat_ngram_size=2,
+            temperature=0.7,
+            top_p=0.7
         )
         decoded_generation = conversation.decoder.gpt_tokenizer.decode(generation[0], skip_special_tokens=True)
 
-        inputs = decoded_generation.split("Based on ")[0]
-        explanation = decoded_generation.split("explain why: ")[1]
+        explanation = decoded_generation.split(instruction)[1]
 
-        return_s += inputs + "<br><b>Explanation:</b> " + explanation
+        return_s += "<b>Original text:</b> " + text \
+                    + "<br><b>Prediction:</b> " + pred_str \
+                    + "<br><b>Explanation:</b> " + explanation
 
     return return_s, 1
 
