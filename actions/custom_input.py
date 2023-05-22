e462620dc694234cbb68c5d44deff1b78b8b4366
qiaw99 <872907968@qq.com>
2023-05-22 09:59:47 +0200

    change cache

--- actions/custom_input.py
+++ actions/custom_input.py
@@ -190,39 +190,21 @@ def generate_explanation(model, dataset_name, inputs, file_name="custom_input"):
         jsonContent = fileObject.read()
         res_list = json.loads(jsonContent)
 
-        if dataset_name == 'boolq':
+        if len(inputs) == 1:
+            for res in res_list:
+                if res["original_text"] == inputs[0]:
+                    return [res]
+        else:
+            cache_text = [i["original_text"] for i in res_list]
+            cache_text_set = set(cache_text)
 
-            if len(inputs) == 1:
-                for res in res_list:
-                    if res["text"] == inputs[0]:
-                        return [res]
-            else:
-                cache_text = [i["text"] for i in res_list]
-                cache_text_set = set(cache_text)
-
-                # If cache contains all inputs
-                if set(inputs).issubset(cache_text_set):
-                    json_list = []
-                    for i in res_list:
-                        if i["text"] in inputs:
-                            json_list.append(i)
-                    return json_list
-        elif dataset_name == 'olid':
-            if len(inputs) == 1:
-                for res in res_list:
-                    if res["text"] == inputs[0]:
-                        return [res]
-            else:
-                cache_text = [i["original_text"] for i in res_list]
-                cache_text_set = set(cache_text)
-
-                # If cache contains all inputs
-                if set(inputs).issubset(cache_text_set):
-                    json_list = []
-                    for i in res_list:
-                        if i["original_text"] in inputs:
-                            json_list.append(i)
-                    return json_list
+            # If cache contains all inputs
+            if set(inputs).issubset(cache_text_set):
+                json_list = []
+                for i in res_list:
+                    if i["original_text"] in inputs:
+                        json_list.append(i)
+                return json_list
 
     dataloader = get_dataloader(inputs, dataset_name)
 
