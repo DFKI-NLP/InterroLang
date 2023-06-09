e5fc5bbe355ee4241af98eae3dde2eead9b7d5ae
qiaw99 <872907968@qq.com>
2023-06-09 12:22:28 +0200

    small fix

--- actions/nlu/similarity.py
+++ actions/nlu/similarity.py
@@ -1,5 +1,5 @@
 import re
-#import emoji
+# import emoji
 from sentence_transformers import SentenceTransformer, util
 
 from timeout import timeout
@@ -29,7 +29,6 @@ def extract_id_number(parse_text):
 
 @timeout(60)
 def similar_instances_operation(conversation, parse_text, i, **kwargs):
-
     """
     Args:
         conversation: conversation object
@@ -42,7 +41,7 @@ def similar_instances_operation(conversation, parse_text, i, **kwargs):
         return "There are no instances that meet this description!", 0
     dataset = conversation.stored_vars["dataset"]
 
-    if conversation.custom_input:
+    if conversation.custom_input is not None and conversation.used is False:
         query = conversation.custom_input
         idx = 0
         number = 3
@@ -50,14 +49,12 @@ def similar_instances_operation(conversation, parse_text, i, **kwargs):
         idx, number = extract_id_number(parse_text)
         query = " ".join(dataset.contents["X"].loc[[idx]].values.tolist()[0])
 
-
     final_results = get_similar_str(query, idx, number, dataset)
 
     return final_results, 1
 
 
 def get_similar_str(query, idx, number, dataset):
-
     """
     Args:
     dataset: dataset from the conversation
@@ -68,21 +65,21 @@ def get_similar_str(query, idx, number, dataset):
     filtered similarity response to a maximum of specified number
     """
     # preparing the output string
-    out_str = "The original text for <b>id "+str(idx)+"</b>:<br>"
+    out_str = "The original text for <b>id " + str(idx) + "</b>:<br>"
     query_tokens = query.split()
     query_preview = " ".join(query_tokens[:16])
-    out_str+= "<summary>"+query_preview+"...</summary><details>"+query+"</details><br>"
-    out_str += "Here are some instances similar to <b>id "+str(idx)+"</b>:<br>"
+    out_str += "<summary>" + query_preview + "...</summary><details>" + query + "</details><br>"
+    out_str += "Here are some instances similar to <b>id " + str(idx) + "</b>:<br>"
     found_similars = get_similars(query, idx, dataset, number)
     for cossim, similar_id, similar in found_similars:
         similar_tokens = similar.split()
         similar_preview = " ".join(similar_tokens[:16])
-        out_str+="<b> id "+str(similar_id)+"</b> (cossim "+str(round(cossim,3))+"):  <summary>"+similar_preview+"...</summary><details>"+similar+"</details><br>"
+        out_str += "<b> id " + str(similar_id) + "</b> (cossim " + str(round(cossim,
+                                                                             3)) + "):  <summary>" + similar_preview + "...</summary><details>" + similar + "</details><br>"
     return out_str
 
 
 def get_similars(query, query_idx, dataset, number):
-
     """
     Args:
     dataset: dataset from the conversation
@@ -99,7 +96,7 @@ def get_similars(query, query_idx, dataset, number):
         if idx != query_idx:
             indices.append(idx)
             texts.append(" ".join(dataset.contents["X"].loc[[idx]].values.tolist()[0]))
-    #TA use caching if the dataset is too big?
+    # TA use caching if the dataset is too big?
     similarity_model = SentenceTransformer("all-MiniLM-L6-v2")
     query_embedding = similarity_model.encode(query)
     sent_embeddings = similarity_model.encode(texts)
@@ -107,13 +104,9 @@ def get_similars(query, query_idx, dataset, number):
     # sort by cossim
     similars = []
     for i in range(len(cos_sim)):
-        similars.append((cos_sim[i],indices[i],texts[i]))
+        similars.append((cos_sim[i], indices[i], texts[i]))
     similars = sorted(similars, key=lambda x: x[0], reverse=True)
     return similars[:number]
-
-
-
-
 
 
 if __name__ == '__main__':
