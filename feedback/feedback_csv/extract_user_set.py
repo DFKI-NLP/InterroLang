3428b3db097a11dfef568dae48f03fd84828f797
qiaw99 <872907968@qq.com>
2023-06-19 09:40:56 +0200

    solve empty line problem

--- feedback/feedback_csv/extract_user_set.py
+++ feedback/feedback_csv/extract_user_set.py
@@ -7,6 +7,8 @@ files = os.listdir(path)
 
 user_set_path = "../../experiments/parsing_interrolang_dev/user_set_interrolang.txt"
 
+is_exist = False
+
 for file in files:
     if file.endswith(".csv"):
         df = pd.read_csv(file)
@@ -15,7 +17,13 @@ for file in files:
 
         assert len(user_text) == len(golden_label)
 
+        if os.path.exists(user_set_path):
+            is_exist = True
+
         with open(user_set_path, "a+") as f:
+            if is_exist:
+                f.write("\n")
+
             for i in range(len(user_text)):
                 f.write(user_text[i] + '\n')
 
